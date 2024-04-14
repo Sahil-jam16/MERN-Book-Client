f898a5e3748be9f8bdac07b1fc28b4005ec1477f
Sahil-jam16 <94698721+Sahil-jam16@users.noreply.github.com>
2024-04-14 14:20:39 +0530

    Add files via upload

---
+++ src/pages/Home/BestSeller.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import BookCards from '../shared/BookCards';
+
+const BestSeller = () => {
+    const [books, setBooks] = useState([]);
+
+//    useEffect(() => {
+//        fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data.slice(0, 8)))
+//    }, [])
+      useEffect(() => {
+          fetch("https://mern-book-server-p7pe.onrender.com/all-books").then(res => res.json()).then(data => setBooks(data.slice(0, 8)))
+       }, [])
+
+    return (
+        <>
+            <BookCards books={books} headline={"Best Seller Books"} />
+        </>
+    )
+}
+
+export default BestSeller
